ea47ebcd24dd9538739e5976941725683c65e5fa
Priyanka <priyanka2001@gmail.com>
2024-09-05 23:16:26 +0530

    Flatlist

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -10,23 +10,30 @@ type ProductListItemProps = {
 }
 
 
-const ProductListItems = ({product}: ProductListItemProps) => {
+const ProductListItem = ({product}: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-    <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} />
+    <Image 
+    source={{ uri: product.image || defaultPizzaImage }} 
+    style={styles.image}
+    resizeMode='contain'
+    />
+
     <Text style={styles.title}>{product.name}</Text>
     <Text style={styles.price}>{product.price}</Text>
   </View>
   )
 }
 
-export default ProductListItems;
+export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth : "50%"
   },
   image: {
     width: '100%',
