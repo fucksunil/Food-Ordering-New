b12ba690601cb460a5db083e3145bc2bde36d5b9
Priyanka <priyanka2001@gmail.com>
2024-09-06 08:33:17 +0530

    Expo Router Screens

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,27 +1,30 @@
 import Colors from '@/src/constants/Colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
-export const defaultPizzaImage = 
-    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+export const defaultPizzaImage =
+  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
 
 type ProductListItemProps = {
-    product : Product;
+  product: Product;
 }
 
 
-const ProductListItem = ({product}: ProductListItemProps) => {
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-    <Image 
-    source={{ uri: product.image || defaultPizzaImage }} 
-    style={styles.image}
-    resizeMode='contain'
-    />
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode='contain'
+        />
 
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>{product.price}</Text>
-  </View>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+      </Pressable>
+    </Link>
   )
 }
 
@@ -33,7 +36,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 20,
     flex: 1,
-    maxWidth : "50%"
+    maxWidth: "50%"
   },
   image: {
     width: '100%',
