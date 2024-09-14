0b6537adabbd697c75a960d03d0ce9e4fb6393fd
Priyanka <priyanka2001@gmail.com>
2024-09-14 15:10:52 +0530

    Assignment 2: Orders

---
+++ src/app/(admin)/orders/[id].tsx
@@ -0,0 +1,33 @@
+import orders from "@/assets/data/orders";
+import OrderItemListItem from "@/src/components/OrderItemListItem";
+import OrderListItem from "@/src/components/OrderListItem";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { FlatList, Text, View } from "react-native";
+
+export default function OrderDetailsScreen() {
+    const { id } = useLocalSearchParams();
+
+    const order = orders.find((o) => o.id.toString() === id)
+
+    if (!order) {
+        return <Text>Not Found</Text>
+    }
+
+    // console.log(order)
+
+    return (
+        <View style={{ padding: 10, gap: 10}}>
+            <Stack.Screen options={{ title: `Order #${id}` }} />
+            
+
+            <FlatList
+                data={order.order_items}
+                renderItem={({ item }) => <OrderItemListItem item={item} />}
+                contentContainerStyle={{ gap: 10 }}
+                ListHeaderComponent={() => <OrderListItem order={order} />}
+            />
+
+        </View>
+
+    )
+}
