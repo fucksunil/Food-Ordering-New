6ca6168d58f3fd146e3abd3f9c131a5a71a10354
Priyanka <priyanka2001@gmail.com>
2024-09-14 19:32:24 +0530

    Authentication

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://yquqdsiczsuysgcnwzdx.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdXFkc2ljenN1eXNnY253emR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzMTk5MzUsImV4cCI6MjA0MTg5NTkzNX0.I88AuhgKFEnliXMcJJRhV1htKE0SlR-r4gDH8cDU5Nw';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
